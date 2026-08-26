513c01d9376d8f920ccfdb85bf291d1156f119c7
Informatica <informatica@alumno.local>
2026-08-26 17:23:16 -0600

    feat: validacion de campos en registro usuario

--- src/org/eliaquimjolon/system/controller/RegisterController.java
+++ src/org/eliaquimjolon/system/controller/RegisterController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import org.eliaquimjolon.system.utils.AlertInformation;
 import org.eliaquimjolon.system.utils.Validations;
 import org.eliaquimjolon.system.utils.ViewFactory;
 /**
@@ -28,7 +29,7 @@ public class RegisterController implements Initializable {
      @FXML private Button btnCancel;
      @FXML private Button btnCreateUser;
      private Validations validate = new Validations();
-     
+     private AlertInformation alertInfo = new AlertInformation();
     
     
     @Override
@@ -42,14 +43,37 @@ public class RegisterController implements Initializable {
         viewFacto.viewLogin();
     }
     
+    
    @FXML
     public void onRegisterUser(MouseEvent event){
         String email = txtEmail.getText().trim();
         boolean validEmail = validate.validateEmail(email);
-        if(validEmail == true )
-            System.out.println("Si esta valido");
-        else
-            System.out.println("No esta valido");
+        if(validEmail == false){
+            alertInfo.viewAlert("ERROR","ERROR DE EMAIL", "ERROR CAMPO EMAIL",
+                            "INGRESASTE UN EMAIL INCORRECTO");
+            return;
+        }
+
+        String user, name, lastName, password, confirmPassword;
+        user = txtUser.getText().trim();
+        name = txtName.getText().trim();
+        lastName = txtLastName.getText();
+        password = pwdPassword.getText().trim();
+        confirmPassword = pwdConfirmPassword.getText().trim();
+        if(validate.validateTextEmpty(user) == true ||
+           validate.validateTextEmpty(name) == true ||
+           validate.validateTextEmpty(email) == true ||
+           validate.validateTextEmpty(lastName) == true ||
+           validate.validateTextEmpty(password) == true ||
+           validate.validateTextEmpty(confirmPassword) == true){
+            
+          alertInfo.viewAlert("ERROR","ERROR DE EMAIL", 
+                  "ERROR CAMPO EMAIL",
+                  "INGRESASTE UN EMAIL INCORRECTO");
+          return;
+          
+        }
+
     }
     
 }
